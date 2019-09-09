25a8b15aa0abb9eae5613b05b347c02fd37ca82b
Joshua I. Colvin <jcolvin2@cams>
2019-09-09 12:38:17 -0500

    Initializing a Python file to start commenting

---
+++ project.py
@@ -0,0 +1,8 @@
+# Software Engineering 49200 - 001
+# Fall 2019
+# Contributers: (Possibly include rolls within project)
+# William Pulkownik
+# Kevin Skulski
+# Edgar Flores
+# Josh Colvin
+print("Welcome to our project")
