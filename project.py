44578d6edecf08d6c9a14d704e87d95732ec0c7d
Cicada1105 <42916703+Cicada1105@users.noreply.github.com>
2019-09-13 11:00:47 -0500

    Update project.py

--- project.py
+++ project.py
@@ -1,6 +1,6 @@
 # Software Engineering 49200 - 001
 # Fall 2019
-# Contributers: (Possibly include rolls within project)
+# Contributers: (Possibly include roles within project)
 # William Pulkownik
 # Kevin Skulski
 # Edgar Flores
