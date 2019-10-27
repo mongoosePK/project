f50ad82290ed4abcb753b55a7604300653cbd12e
Cicada1105 <guitarlegen@gmail.com>
2019-10-27 18:09:04 -0500

    Finished testing all of the queries and organization the hierarchy of all of the classes and how they work together

--- mysite/modules/RequestBuilder.py
+++ mysite/modules/RequestBuilder.py
@@ -1,78 +1,16 @@
-'This file is used to create a request to be sent to the IMDb database'
-# imports
-import sys
+'''
+    This file is used to create requests to be sent to the IMDb database
+    based on request type
+'''
 
-# Include module path for python to search
-sys.path.append("../dependencies/")
-
-import requests
-
-class Request:
-    _api = "84e37cb7"
-    _domain = "http://www.omdbapi.com/?"
-
-    def __init__(self):
-        self._json = ""
-        self._title = ""
-        self._year = None
-        self._search_type = ""
-
-        # Create base of url for all IMDb requests
-        self.url = Request._domain + "apikey=" + Request._api
-        
-    def makeRequest(self):
-        # Make request to current url
-        # Send request
-        response = requests.get(self.url)
-        
-        #and (response.json()["Response"] == "True")
-        if (response.status_code == 200):
-            if (response.json()["Response"] == 'True'):
-                print("Success")
-                self._json = response.json()
-            else:
-                print("Unable to find Request")
-        elif response.status_code == 404:
-            print("Unable to return resource")
-        else:
-            print(response.status_code)
-
-    '''
-        Setters for data controlled by url and
-        shared by all Requests
-    '''
-    def setTitle(self, tIn):
-        self._title = tIn
-    def setSearchType(self,srcType):
-        # Search type can be: movie, series, episode
-        self._search_type = srcType
-    def setYear(self, yIn):
-        self._year = yIn
-
-    # Allow user to collect search title stored
-    def getTitle(self):
-        return self._title
-    # Allow user to collect search type stored
-    def getSearchType(self):
-        return self._search_type
-    # Allow user to collect year of current Request
-    def getYear(self):
-        return self._year
-    # Allow user to collect data
-    def getJSON(self):
-        return self._json
-            
-class SearchRequest(Request):
+import Request
+class SearchRequest(Request.RequestHandler):
     def __init__(self, title = "", year = None):
         # Call to parent class
-        super().__init__()
-
-        self.setTitle(title)
-        self.setYear(year)
+        super().__init__(title, year)
         
         # Which page to return out of 100
         self._page_num = 1 # Default page: 1, max: 100
-        
     '''
         Setters for data controlled by url and
         specific to the SearchReqest class
@@ -88,25 +26,23 @@ class SearchRequest(Request):
         # Make request by calling parent function
         super().makeRequest()
     
-    def _build_search_url(self):        
-        if self.getTitle() != "":
-            self.url = "%s&s=%s" % (self.url, self.getTitle())
-        if self.getYear() != None:
-            self.url = "%s&y=%d" % (self.url, self.getYear())
-        if self.getSearchType() != "":
+    def _build_search_url(self):
+        if self._title != "":
+            self.url = "%s&s=%s" % (self.url, self._title)
+        if self._year != None:
+            self.url = "%s&y=%d" % (self.url, self._year)
+        if self._search_type != "":
             #append search type to each url
-            self.url = "%s&type=%s" % (self.url, self.getSearchType())
+            self.url = "%s&type=%s" % (self.url, self._search_type)
 
         # append page number to each url
         self.url = "%s&page=%d" % (self.url, self._page_num)
         
-class MovieRequest(Request):
+class MovieRequest(Request.RequestHandler):
     def __init__(self, title = "", year = None):
         # Call to parent class
-        super().__init__()
-
-        self.setTitle(title)
-        self.setYear(year)
+        super().__init__(title, year)
+        
         self._movieID = ""
         self._plot_type = ""
 
@@ -130,30 +66,36 @@ class MovieRequest(Request):
             
     def _build_movie_url(self):
 
-        if self.getTitle() != "":
-            self.url = "%s&t=*%s" % (self.url, self.getTitle())
+        if self._title != "":
+            self.url = "%s&t=*%s" % (self.url, self._title)
         if self._movieID != "":
             self.url = "%s&i=%s" % (self.url, self._movieID)
-        if self.getYear() != None:
-            self.url = "%s&y=%d" % (self.url, self.getYear())
-        if self.getSearchType() != "":
-            self.url = "%s&type=%s" % (self.url, self.getSearchType())
+        if self._year != None:
+            self.url = "%s&y=%d" % (self.url, self._year)
+        if self._search_type != "":
+            self.url = "%s&type=%s" % (self.url, self._search_type)
         if self._plot_type != "":
             self.url = "%s&plot=%s" % (self.url, self._plot_type)
         
 
 if __name__ == "__main__":
-    req1 = MovieRequest("8 mile")
+    '''
+    req1 = MovieRequest()
     req1.setMovieId("tt0298203") # imdbID for 8 mile
     req1.setSearchType("movie")
     req1.setPlotType("short")
     #make request
     req1.makeRequest()
-    jsonData = req1.getJSON()
-    print(jsonData["Title"])
-
-    req2 = SearchRequest("Guardians")
+    print(req1.getTitle())
+    print(req1.getRated())
+    print(req1.getGenre())
+    print(req1.getPlot())
+    '''
+    '''
+    req2 = SearchRequest()
+    req2.setTitle("Guardians")
     req2.setSearchType("movie")
     req2.makeRequest()
-    jsonData = req2.getJSON()
-    print(jsonData["Search"][0]["Title"])
+    for i in range(0, 10):
+        print(req2.getTitle(i))
+    '''
