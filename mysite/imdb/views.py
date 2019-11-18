cdfe3fd723c2e4e0c94c635209d937575086c26c
Joshua Colvin <joshuacolvin@Joshuas-MacBook-Pro.local>
2019-11-18 00:26:12 -0600

    Formatted returned movies to be cleaner

--- mysite/imdb/views.py
+++ mysite/imdb/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+
 import sys
 sys.path.append("imdb/")
 # include model to handle Movie Requests
-from models import MovieModel
+#from models import MovieModel
+from imdb_dummy_data import MOVIE_REQUEST_DUMMY_DATA
 
 # Create your views here.
 @login_required
 def home(request):
-    genreData = MovieModel("horror")
-    return render(request, "movie_display.html", {"currUserData": genreData.getData()})
+    #genreData = MovieModel("horror")
+    movie_list = (MOVIE_REQUEST_DUMMY_DATA)
+    return render(request, "movie_display.html", {"searchResult": movie_list,"userSearch":"Guardians"})#genreData.getData()})
+
